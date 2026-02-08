01b74e4c079ff51430410312696396d1e9575640
ChristieBoz <152922237+ChristieBoz@users.noreply.github.com>
2026-02-08 13:13:05 -0500

    Create customer_landing external table
    Create an external table for customer landing data with specified fields and properties.

--- sql/customer_landing.sql
+++ sql/customer_landing.sql
@@ -1,1 +1,19 @@
--- customer landing ddl 
+CREATE EXTERNAL TABLE IF NOT EXISTS stedi.customer_landing (
+  customername              string,
+  email                     string,
+  phone                     string,
+  birthday                  string,
+  serialnumber              string,
+  registrationdate          bigint,
+  lastupdatedate            bigint,
+  sharewithresearchasofdate bigint,
+  sharewithpublicasofdate   bigint,
+  sharewithfriendsasofdate  bigint
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'ignore.malformed.json' = 'true'
+)
+LOCATION 's3://christina-stedi-datalake/landing/customer/'
+TBLPROPERTIES ('has_encrypted_data'='false');
+
