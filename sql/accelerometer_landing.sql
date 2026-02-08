01e5f8101d4a9563a9b2d1602597e3f826336e96
ChristieBoz <152922237+ChristieBoz@users.noreply.github.com>
2026-02-08 13:20:54 -0500

    Create external table for accelerometer data

--- sql/accelerometer_landing.sql
+++ sql/accelerometer_landing.sql
@@ -1,1 +1,14 @@
--- accelerometer_landing
+CREATE EXTERNAL TABLE IF NOT EXISTS stedi.accelerometer_landing (
+  timestamp string,
+  user      string,
+  x         double,
+  y         double,
+  z         double
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'ignore.malformed.json' = 'true'
+)
+LOCATION 's3://christina-stedi-datalake/landing/accelerometer/'
+TBLPROPERTIES ('has_encrypted_data'='false');
+
