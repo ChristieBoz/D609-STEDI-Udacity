bf5e7229787dccb4b26e514a7b2627baa7a086c5
ChristieBoz <152922237+ChristieBoz@users.noreply.github.com>
2026-02-08 18:05:34 -0500

    Update join operation and add serialnumber key

--- glue_jobs/machine_learning_curated.py
+++ glue_jobs/machine_learning_curated.py
@@ -8,10 +8,9 @@ from awsglue.dynamicframe import DynamicFrame
 
 from pyspark.sql.functions import col, floor
 
-# Read JOB_NAME from args
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 
-# Set up contexts
+
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
@@ -19,19 +18,17 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# 1. Read trusted step trainer data
 step_trainer_trusted_dyf = glueContext.create_dynamic_frame.from_catalog(
     database="stedi",
     table_name="step_trainer_trusted"
 )
 
-# 2. Read trusted accelerometer data
+
 accelerometer_trusted_dyf = glueContext.create_dynamic_frame.from_catalog(
     database="stedi",
     table_name="accelerometer_trusted"
 )
 
-# 3. Convert to DataFrames and create "time_sec" (timestamp rounded to seconds)
 step_df = step_trainer_trusted_dyf.toDF().withColumn(
     "time_sec",
     (col("sensorreadingtime").cast("bigint") / 1000).cast("bigint")
@@ -42,14 +39,12 @@ accel_df = accelerometer_trusted_dyf.toDF().withColumn(
     (col("timestamp").cast("bigint") / 1000).cast("bigint")
 )
 
-# 4. Join on time_sec
 joined_df = step_df.join(
     accel_df,
-    on="time_sec",
+    on=["time_sec", "serialnumber"], 
     how="inner"
 )
 
-# 5. Select the ML fields we care about
 ml_df = joined_df.select(
     col("sensorreadingtime"),
     col("serialnumber"),
@@ -59,14 +54,13 @@ ml_df = joined_df.select(
     col("z")
 )
 
-# 6. Convert back to DynamicFrame
 ml_curated_dyf = DynamicFrame.fromDF(
     ml_df,
     glueContext,
     "ml_curated_dyf"
 )
 
-# 7. Sink: write to S3 and update Glue Catalog
+
 sink = glueContext.getSink(
     path="s3://christina-stedi-datalake/curated/machine_learning/",
     connection_type="s3",
