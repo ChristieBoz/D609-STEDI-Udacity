a0de63da4295aa38d519c9acb8ffb3200af04a0c
ChristieBoz <152922237+ChristieBoz@users.noreply.github.com>
2026-02-08 16:36:17 -0500

    Implement Glue job for accelerometer data processing

--- glue_jobs/accelerometer_landing_to_trusted.py
+++ glue_jobs/accelerometer_landing_to_trusted.py
@@ -1,1 +1,71 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
 
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+
+accel_landing_dyf = glueContext.create_dynamic_frame.from_catalog(
+    database="stedi",
+    table_name="accelerometer_landing"
+)
+
+
+customer_trusted_dyf = glueContext.create_dynamic_frame.from_catalog(
+    database="stedi",
+    table_name="customer_trusted"
+)
+
+
+joined_dyf = Join.apply(
+    frame1=accel_landing_dyf,
+    frame2=customer_trusted_dyf,
+    keys1=["user"],
+    keys2=["email"]
+)
+
+accel_trusted_dyf = DropFields.apply(
+    frame=joined_dyf,
+    paths=[
+        "customerName",
+        "email",
+        "phone",
+        "birthDay",
+        "serialNumber",
+        "registrationDate",
+        "lastUpdateDate",
+        "shareWithResearchAsOfDate",
+        "shareWithPublicAsOfDate",
+        "shareWithFriendsAsOfDate"
+    ]
+)
+
+
+sink = glueContext.getSink(
+    path="s3://christina-stedi-datalake/trusted/accelerometer/",
+    connection_type="s3",
+    updateBehavior="UPDATE_IN_DATABASE",
+    enableUpdateCatalog=True,
+    partitionKeys=[]
+)
+
+sink.setFormat("glueparquet")
+
+sink.setCatalogInfo(
+    catalogDatabase="stedi",
+    catalogTableName="accelerometer_trusted"
+)
+
+sink.writeFrame(accel_trusted_dyf)
+
+job.commit()
