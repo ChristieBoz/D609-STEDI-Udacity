42d6f7bf8572dd54bb7bf03e03cce7ae8f237e68
ChristieBoz <152922237+ChristieBoz@users.noreply.github.com>
2026-02-08 17:59:57 -0500

    Enhance data processing for ML with joins and selections
    Added data processing steps to join step trainer and accelerometer data, convert timestamps, and select relevant ML fields.

--- glue_jobs/machine_learning_curated.py
+++ glue_jobs/machine_learning_curated.py
@@ -4,9 +4,14 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
 
+from pyspark.sql.functions import col, floor
+
+# Read JOB_NAME from args
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 
+# Set up contexts
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
@@ -14,35 +19,54 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
+# 1. Read trusted step trainer data
 step_trainer_trusted_dyf = glueContext.create_dynamic_frame.from_catalog(
     database="stedi",
     table_name="step_trainer_trusted"
 )
 
+# 2. Read trusted accelerometer data
 accelerometer_trusted_dyf = glueContext.create_dynamic_frame.from_catalog(
     database="stedi",
     table_name="accelerometer_trusted"
 )
 
-step_accel_joined_dyf = Join.apply(
-    frame1=step_trainer_trusted_dyf,
-    frame2=accelerometer_trusted_dyf,
-    keys1=["sensorreadingtime"],
-    keys2=["timestamp"]
+# 3. Convert to DataFrames and create "time_sec" (timestamp rounded to seconds)
+step_df = step_trainer_trusted_dyf.toDF().withColumn(
+    "time_sec",
+    (col("sensorreadingtime").cast("bigint") / 1000).cast("bigint")
 )
 
-ml_curated_dyf = SelectFields.apply(
-    frame=step_accel_joined_dyf,
-    paths=[
-        "sensorreadingtime",
-        "serialnumber",
-        "distancefromobject",
-        "x",
-        "y",
-        "z"
-    ]
+accel_df = accelerometer_trusted_dyf.toDF().withColumn(
+    "time_sec",
+    (col("timestamp").cast("bigint") / 1000).cast("bigint")
 )
 
+# 4. Join on time_sec
+joined_df = step_df.join(
+    accel_df,
+    on="time_sec",
+    how="inner"
+)
+
+# 5. Select the ML fields we care about
+ml_df = joined_df.select(
+    col("sensorreadingtime"),
+    col("serialnumber"),
+    col("distancefromobject"),
+    col("x"),
+    col("y"),
+    col("z")
+)
+
+# 6. Convert back to DynamicFrame
+ml_curated_dyf = DynamicFrame.fromDF(
+    ml_df,
+    glueContext,
+    "ml_curated_dyf"
+)
+
+# 7. Sink: write to S3 and update Glue Catalog
 sink = glueContext.getSink(
     path="s3://christina-stedi-datalake/curated/machine_learning/",
     connection_type="s3",
