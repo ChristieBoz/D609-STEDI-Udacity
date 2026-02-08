f8746974d18c4cc3b63aa8911e1357f5b9bda142
ChristieBoz <152922237+ChristieBoz@users.noreply.github.com>
2026-02-08 14:11:33 -0500

    Create external table for step trainer landing data

--- sql/step_trainer_landing.sql
+++ sql/step_trainer_landing.sql
@@ -1,1 +1,12 @@
--- step_trainer_landing.
+CREATE EXTERNAL TABLE IF NOT EXISTS stedi.step_trainer_landing (
+  sensorreadingtime   string,
+  serialnumber        string, 
+  distancefromobject  double
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'ignore.malformed.json' = 'true'
+)
+LOCATION 's3://christina-stedi-datalake/landing/step_trainer/'
+TBLPROPERTIES ('has_encrypted_data'='false');
+
